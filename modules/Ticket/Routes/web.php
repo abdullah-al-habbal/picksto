23f774d0fcfd49a71b07d79d691c963328681123
Abdullah Al Habal <dbnkalhbalb@outlook.sa>
2026-04-28 11:32:21 +0300

    fix: remove deprecated admin ticket routes -> moved to Filament TicketResource

--- modules/Ticket/Routes/web.php
+++ modules/Ticket/Routes/web.php
@@ -7,23 +7,12 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 use Modules\Ticket\Http\Actions\AddReplyAction;
 use Modules\Ticket\Http\Actions\CreateTicketAction;
-use Modules\Ticket\Http\Actions\DeleteTicketAction;
-use Modules\Ticket\Http\Actions\GetAllTicketsAction;
 use Modules\Ticket\Http\Actions\GetTicketDetailsAction;
-use Modules\Ticket\Http\Actions\GetTicketsStatsAction;
 use Modules\Ticket\Http\Actions\GetUserTicketsAction;
-use Modules\Ticket\Http\Actions\UpdateTicketStatusAction;
 
 Route::middleware('auth')->prefix('tickets')->name('tickets.')->group(static function (): void {
     Route::post('/', CreateTicketAction::class)->name('store');
     Route::get('my-tickets', GetUserTicketsAction::class)->name('my-tickets');
     Route::get('{ticket}', GetTicketDetailsAction::class)->name('show');
     Route::post('{ticket}/reply', AddReplyAction::class)->name('reply');
-});
-
-Route::middleware(['auth', 'role:admin'])->prefix('admin/tickets')->name('admin.tickets.')->group(static function (): void {
-    Route::get('stats/overview', GetTicketsStatsAction::class)->name('stats');
-    Route::get('/', GetAllTicketsAction::class)->name('index');
-    Route::put('{ticket}/status', UpdateTicketStatusAction::class)->name('status.update');
-    Route::delete('{ticket}', DeleteTicketAction::class)->name('destroy');
 });
